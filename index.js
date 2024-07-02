4da54a5d9edce035aad5026e584ef360b18dd0e9
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-02 18:19:24 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let multiply;
+const num1=31
+const num2=2
+ multiply=num1*num2
+ console.log(multiply)
+
+
+let random=42
+random=parseInt(random)
+console.log(random)
+
+let mod;
+const num3=59
+const num4=5
+mod=num3%num4
+console.log(mod)
+
+let max;
+max=Math.max(11,12,13,14,15,16,17,18,19,20)
+console.log(numb)
